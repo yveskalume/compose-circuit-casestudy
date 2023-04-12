903ffe7841bc4949b8fff92df493eb84fa38fffc
yveskalume <yvkalume@gmail.com>
2023-04-12 23:34:04 +0200

    fix dao

--- app/src/main/java/com/yveskalume/circuitcasestudy/data/FruitDao.kt
+++ app/src/main/java/com/yveskalume/circuitcasestudy/data/FruitDao.kt
@@ -1,10 +1,12 @@
 package com.yveskalume.circuitcasestudy.data
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface FruitDao {
 
     @Insert
